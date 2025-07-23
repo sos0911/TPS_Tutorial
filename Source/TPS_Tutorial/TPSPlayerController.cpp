12aa4040ce142f90e3245b8837c43878eab8e6f9
Donghyun LEE <sos0911@korea.ac.kr>
2025-07-24 01:30:20 +0900

    Initial commit

---
+++ Source/TPS_Tutorial/TPSPlayerController.cpp
@@ -0,0 +1,5 @@
+// 커스텀 테스트용 플레이어 컨트롤러 소스 파일
+
+
+#include "TPSPlayerController.h"
+
---
+++ Source/TPS_Tutorial/TPSPlayerController.h
@@ -0,0 +1,17 @@
+// 커스텀 테스트용 플레이어 컨트롤러 헤더
+
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TPSPlayerController.generated.h"
+
+
+UCLASS()
+class TPS_TUTORIAL_API ATPSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
