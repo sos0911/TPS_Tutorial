a5eb737077424797029407a1f5886addbcd13f9b
Donghyun LEE <sos0911@korea.ac.kr>
2025-08-06 01:44:28 +0900

    ABP 및 클래스 생성 / 처리  - BP상 로코모션 작업  - behavior tree 작업

---
+++ Source/TPS_Tutorial/AnimInstance/TPSAnimInstance.cpp
@@ -0,0 +1,4 @@
+﻿// TPS 애님 인스턴스 소스 파일
+
+
+#include "TPSAnimInstance.h"
---
+++ Source/TPS_Tutorial/AnimInstance/TPSAnimInstance.h
@@ -0,0 +1,17 @@
+﻿// TPS 애님 인스턴스 헤더 파일
+
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TPSAnimInstance.generated.h"
+
+
+UCLASS()
+class TPS_TUTORIAL_API UTPSAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+};
