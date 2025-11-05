f9511274fa842cab0540b9d29b78b32c3f3c7db3
sos0911 <sos0911@korea.ac.kr>
2025-11-05 18:08:31 +0900

    HUD UI 준비 및 커스텀 Game Instance 생성  - UI 매니저 따로 만들 계획 중

---
+++ Source/TPS_Tutorial/GameInstance/TPSGameInstance.cpp
@@ -0,0 +1,5 @@
+// 커스텀 게임 인스터스 소스 파일
+
+
+#include "GameInstance/TPSGameInstance.h"
+
---
+++ Source/TPS_Tutorial/GameInstance/TPSGameInstance.h
@@ -0,0 +1,17 @@
+// 커스텀 게임 인스터스 헤더 파일
+
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TPSGameInstance.generated.h"
+
+
+UCLASS()
+class TPS_TUTORIAL_API UTPSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
